234f6c99805a6c16135580b6848eff8258c10376
Ori Broda <orib@pcentra.com>
2018-09-04 15:31:54 +0000

    Regulrization init

--- ex2_logistic_regression/mapFeature.py
+++ ex2_logistic_regression/mapFeature.py
@@ -1,21 +1,22 @@
-function out = mapFeature(X1, X2)
-% MAPFEATURE Feature mapping function to polynomial features
-%
-%   MAPFEATURE(X1, X2) maps the two input features
-%   to quadratic features used in the regularization exercise.
-%
-%   Returns a new feature array with more features, comprising of 
-%   X1, X2, X1.^2, X2.^2, X1*X2, X1*X2.^2, etc..
-%
-%   Inputs X1, X2 must be the same size
-%
+# % MAPFEATURE Feature mapping function to polynomial features
+# %
+# %   MAPFEATURE(X1, X2) maps the two input features
+# %   to quadratic features used in the regularization exercise.
+# %
+# %   Returns a new feature array with more features, comprising of
+# %   X1, X2, X1.^2, X2.^2, X1*X2, X1*X2.^2, etc..
+# %
+# %   Inputs X1, X2 must be the same size
+# %
 
-degree = 6;
-out = ones(size(X1(:,1)));
-for i = 1:degree
-    for j = 0:i
-        out(:, end+1) = (X1.^(i-j)).*(X2.^j);
-    end
-end
+import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
 
-end
+def mapFeature(X1, X2):
+    X = np.column_stack((X1, X2))
+    return PolynomialFeatures(degree=6).fit_transform(X)
+
+X1 = np.arange(1,5)
+X2 = np.arange(-5,-1)
+mapFeature(X1, X2)
+
