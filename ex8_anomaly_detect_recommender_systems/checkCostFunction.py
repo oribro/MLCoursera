f45773a011932be1ee256ebe480b89885dcd0485
Ori Broda <orib@pcentra.com>
2018-10-19 22:05:39 +0000

    Added recommender system for movies

--- ex8_anomaly_detect_recommender_systems/checkCostFunction.py
+++ ex8_anomaly_detect_recommender_systems/checkCostFunction.py
@@ -1,48 +1,93 @@
-function checkCostFunction(lambda)
-%CHECKCOSTFUNCTION Creates a collaborative filering problem 
-%to check your cost function and gradients
-%   CHECKCOSTFUNCTION(lambda) Creates a collaborative filering problem 
-%   to check your cost function and gradients, it will output the 
-%   analytical gradients produced by your code and the numerical gradients 
-%   (computed using computeNumericalGradient). These two gradient 
-%   computations should result in very similar values.
+# %CHECKCOSTFUNCTION Creates a collaborative filering problem
+# %to check your cost function and gradients
+# %   CHECKCOSTFUNCTION(lambda) Creates a collaborative filering problem
+# %   to check your cost function and gradients, it will output the
+# %   analytical gradients produced by your code and the numerical gradients
+# %   (computed using computeNumericalGradient). These two gradient
+# %   computations should result in very similar values.
+#
+# % Set lambda
+# if ~exist('lambda', 'var') || isempty(lambda)
+#     lambda = 0;
+# end
+#
+# %% Create small problem
+# X_t = rand(4, 3);
+# Theta_t = rand(5, 3);
+#
+# % Zap out most entries
+# Y = X_t * Theta_t';
+# Y(rand(size(Y)) > 0.5) = 0;
+# R = zeros(size(Y));
+# R(Y ~= 0) = 1;
+#
+# %% Run Gradient Checking
+# X = randn(size(X_t));
+# Theta = randn(size(Theta_t));
+# num_users = size(Y, 2);
+# num_movies = size(Y, 1);
+# num_features = size(Theta_t, 2);
+#
+# numgrad = computeNumericalGradient( ...
+#                 @(t) cofiCostFunc(t, Y, R, num_users, num_movies, ...
+#                                 num_features, lambda), [X(:); Theta(:)]);
+#
+# [cost, grad] = cofiCostFunc([X(:); Theta(:)],  Y, R, num_users, ...
+#                           num_movies, num_features, lambda);
+#
+# disp([numgrad grad]);
+# fprintf(['The above two columns you get should be very similar.\n' ...
+#          '(Left-Your Numerical Gradient, Right-Analytical Gradient)\n\n']);
+#
+# diff = norm(numgrad-grad)/norm(numgrad+grad);
+# fprintf(['If your cost function implementation is correct, then \n' ...
+#          'the relative difference will be small (less than 1e-9). \n' ...
+#          '\nRelative Difference: %g\n'], diff);
+#
+# end
 
-% Set lambda
-if ~exist('lambda', 'var') || isempty(lambda)
-    lambda = 0;
-end
+import numpy as np
+from ex4_neural_networks_learning.computeNumericalGradient import computeNumericalGradient
+from ex8_anomaly_detect_recommender_systems.cofiCostFunc import cofiCostFunc
 
-%% Create small problem
-X_t = rand(4, 3);
-Theta_t = rand(5, 3);
 
-% Zap out most entries
-Y = X_t * Theta_t';
-Y(rand(size(Y)) > 0.5) = 0;
-R = zeros(size(Y));
-R(Y ~= 0) = 1;
+def checkCostFunction(L = 0):
+    X_t = np.random.rand(4, 3)
+    Theta_t = np.random.rand(5, 3)
+    Y = np.dot(X_t, Theta_t.T)
+    Y[Y > 0.5] = 0
+    R = np.zeros(Y.shape)
+    R[Y != 0] = 1
 
-%% Run Gradient Checking
-X = randn(size(X_t));
-Theta = randn(size(Theta_t));
-num_users = size(Y, 2);
-num_movies = size(Y, 1);
-num_features = size(Theta_t, 2);
+    X = np.random.randn(*X_t.shape)
+    Theta = np.random.randn(*Theta_t.shape)
+    num_movies, num_users= Y.shape
+    num_features = Theta_t.shape[1]
+    costFunc = lambda p: cofiCostFunc(
+        p,
+        Y,
+        R,
+        num_users,
+        num_movies,
+        num_features,
+        L
+    )
+    params = np.concatenate((X.flatten(), Theta.flatten()))
+    cost, grad = costFunc(params)
+    numgrad = computeNumericalGradient(costFunc, params)
 
-numgrad = computeNumericalGradient( ...
-                @(t) cofiCostFunc(t, Y, R, num_users, num_movies, ...
-                                num_features, lambda), [X(:); Theta(:)]);
+    np.set_printoptions(precision=12, suppress=True)
+    print((numgrad, grad))
 
-[cost, grad] = cofiCostFunc([X(:); Theta(:)],  Y, R, num_users, ...
-                          num_movies, num_features, lambda);
+    print('The above two columns you get should be very similar.\n'
+             '(Left-Your Numerical Gradient, Right-Analytical Gradient)\n\n')
 
-disp([numgrad grad]);
-fprintf(['The above two columns you get should be very similar.\n' ...
-         '(Left-Your Numerical Gradient, Right-Analytical Gradient)\n\n']);
+    diff = np.linalg.norm(numgrad - grad) / np.linalg.norm(numgrad + grad)
+    assert diff < 10 ** -9
 
-diff = norm(numgrad-grad)/norm(numgrad+grad);
-fprintf(['If your cost function implementation is correct, then \n' ...
-         'the relative difference will be small (less than 1e-9). \n' ...
-         '\nRelative Difference: %g\n'], diff);
+    print('If your cost function implementation is correct, then \n'
+           'the relative difference will be small (less than 1e-9). \n'
+             '\nRelative Difference: {}\n'.format(diff))
 
-end
+
+
--- ex8_anomaly_detect_recommender_systems/loadMovieList.py
+++ ex8_anomaly_detect_recommender_systems/loadMovieList.py
@@ -1,25 +1,32 @@
-function movieList = loadMovieList()
-%GETMOVIELIST reads the fixed movie list in movie.txt and returns a
-%cell array of the words
-%   movieList = GETMOVIELIST() reads the fixed movie list in movie.txt 
-%   and returns a cell array of the words in movieList.
+# %GETMOVIELIST reads the fixed movie list in movie.txt and returns a
+# %cell array of the words
+# %   movieList = GETMOVIELIST() reads the fixed movie list in movie.txt
+# %   and returns a cell array of the words in movieList.
+#
+#
+# %% Read the fixed movieulary list
+# fid = fopen('movie_ids.txt');
+#
+# % Store all movies in cell array movie{}
+# n = 1682;  % Total number of movies
+#
+# movieList = cell(n, 1);
+# for i = 1:n
+#     % Read line
+#     line = fgets(fid);
+#     % Word Index (can ignore since it will be = i)
+#     [idx, movieName] = strtok(line, ' ');
+#     % Actual Word
+#     movieList{i} = strtrim(movieName);
+# end
+# fclose(fid);
+#
+# end
 
 
-%% Read the fixed movieulary list
-fid = fopen('movie_ids.txt');
+def loadMovieList():
+    with open('movie_ids.txt', 'r', encoding='ISO-8859-1') as movies:
+        lines = movies.readlines()
+        movie_titles = [line.strip().split(' ', 1)[1] for line in lines]
 
-% Store all movies in cell array movie{}
-n = 1682;  % Total number of movies 
-
-movieList = cell(n, 1);
-for i = 1:n
-    % Read line
-    line = fgets(fid);
-    % Word Index (can ignore since it will be = i)
-    [idx, movieName] = strtok(line, ' ');
-    % Actual Word
-    movieList{i} = strtrim(movieName);
-end
-fclose(fid);
-
-end
+    return movie_titles
